6f24c8719c526161051b0d0cb8cb9a637ac6dd0e
Michael Hsieh <mdhsieh8@gmail.com>
2023-09-08 13:24:27 -0700

    Initial commit.
    New SwiftUI app and gitignore file.

---
+++ Sign In Options Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sign In Options Example
+//
+//  Created by Michael Hsieh on 9/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sign In Options Example/Sign_In_Options_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sign_In_Options_ExampleApp.swift
+//  Sign In Options Example
+//
+//  Created by Michael Hsieh on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Sign_In_Options_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
